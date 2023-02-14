77cf0c92fa1fad767b5ae73ca14f8efedd215c35
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2023-02-15 00:17:35 +0100

    Added MemoryOffset

--- base/src/types.hpp
+++ base/src/types.hpp
@@ -1,7 +1,7 @@
 #ifndef NERD_BASE_TYPES_HPP
 #define NERD_BASE_TYPES_HPP
 
-// This file shamelessly ripped off from RedEngine RedStrain
+// This file semi-shamelessly semi-ripped-off from RedEngine RedStrain
 
 #include "IntegerTypeByTraits.hpp"
 
@@ -9,6 +9,7 @@ namespace nerd {
 namespace base {
 
 	typedef IntegerPointer MemorySize;
+	typedef PointerOffset MemoryOffset;
 	typedef uint64_t FileSize;
 	typedef int64_t FileOffset;
 
